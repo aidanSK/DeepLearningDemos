6990cf29a0a94bbf176e3665990d933141f43398
Aidan Kayes <111781841+aidanSK@users.noreply.github.com>
2023-12-24 02:01:44 -0500

    Add files via upload

---
+++ FlaskApp/flaskApp.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+#pip install flask
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    #Retrieves parameters from URL, defaulting to None type if nothing is given
+    condition = request.args.get('condition', None)
+
+    if condition == None:
+        return "No arguments given"
+    elif condition == 'Rowan':
+        return "Hello Rowan!"
+    else:
+        return "Hello World!"
+
+#hostname: localhost
+#default port: 8000
+#url: localhost:8000/hello
+#with params: localhost:8000/hello?option=0
+
+if __name__ == '__main__':
+    app.run(debug=False, port=8000, host="0.0.0.0")
