df299d2b3c078b1f82c96d1ff6c4e732dee2dfe6
Aidan Kayes <111781841+aidanSK@users.noreply.github.com>
2023-12-27 18:27:19 -0500

    Update flaskApp.py

--- FlaskApp/flaskApp.py
+++ FlaskApp/flaskApp.py
@@ -21,4 +21,4 @@ def hello():
 #with params: localhost:8000/hello?option=0
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8000, host="0.0.0.0")
+    app.run(debug=False, port=8000, host="localhost")
